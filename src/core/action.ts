a8f238f39e1dac99c07bbc7dbcc51e6f6cf47811
FYQ <Fanyinqian@guigu.com>
2023-08-30 23:30:34 +0800

    feat:完善saveJSON命令行

--- src/core/action.ts
+++ src/core/action.ts
@@ -1,9 +1,26 @@
 const inquirer = require("inquirer");
 const Loading = require("../utils/loading");
-const Saving = require("../utils/saving");
 // @ts-ignore
 const chalk = require("chalk");
 const HOST = "8243";
+
+/**
+ * 格式化路径以 .json 为后缀
+ * @param path 文件路径
+ * @returns {string} 后缀为 .json 的文件路径
+ */
+const ensureJsonExtension = (path:string)=>{
+  const extension:string = '.json';
+  // 检查路径是否已经以.json结尾
+  if(!path.endsWith(extension)){
+   // 移除可能的现有后缀
+   const pathWithoutExtension:string = path.replace(/\.[^/.]+$/, "");
+
+   // 加上 .json 后缀
+   path = pathWithoutExtension + extension;
+  }
+  return path;
+}
 
 /**
  * 定义cli为命令提供的处理逻辑
@@ -11,7 +28,7 @@ const HOST = "8243";
 const Action = {
   async analyze(num: number) {
     // 分析依赖，生成依赖树..
-    const res = await Loading(num);
+    await Loading(num);
     // 打开内置网页
     console.log(
       `${chalk.green.bold("➜")}  Open:  ${chalk.blueBright.bold(
@@ -50,9 +67,9 @@ const Action = {
       this.analyze(depth);
     }
   },
-  saveJSON(path: any) {
-    let targetPath = path.json;
-    if (targetPath === true) {
+  async saveJSON(path:Array<string>) { 
+    // 判断是否有路径参数
+    if (!path[3]) {
       inquirer
         .prompt([
           {
@@ -61,15 +78,30 @@ const Action = {
             message: "请输入要保存的位置路径：",
           },
         ])
-        .then(async (save: { path: any }) => {
-          console.log("当前保存路径为:", save.path);
-          Saving(save.path);
+        .then(async (save: { path: string }) => {
+          console.log("当前保存路径为:", chalk.yellow.bold(ensureJsonExtension(save.path)));
+          // 分析依赖，保存本地文件..
+          await Loading(save.path);
         });
     } else {
-      targetPath = targetPath.slice(1, -1);
-      console.log("当前保存路径为:", targetPath);
-      Saving(targetPath);
+      // 如果输入的路径不包含 []
+      if((!path[path.length-1].includes('['))||(!path[path.length-1].includes(']'))){
+        console.log('请按照正确的格式填写文件路径（示例：dependency-tree-cli -j [./test.json]）');
+      }else{
+        // 去除 []，格式化为以 .json 后缀
+        let targetPath = path[path.length-1].replace(/\[|\]/g, "")
+        targetPath = ensureJsonExtension(targetPath)
+        console.log("当前保存路径为:", chalk.yellow.bold(targetPath));
+        await Loading(targetPath)
+      }
     }
+    
+    
+    
+   
+    // console.log(targetPath);
+    
+   
   },
 };
 
